179581d02bdc07fa5102f69009dd1bddbb633112
lazyfoxx96 <lazyfoxx96@gmail.com>
2025-09-02 11:44:58 +0900

    Font추가, 스크립트 추가

---
+++ Assets/Scripts/MainPanelController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MainPanelController : MonoBehaviour
+{
+    public void OnClickSinglePlayButton()
+    {
+
+    }
+
+    public void OnClickDualPlayButton()
+    {
+
+    }
+
+    public void OnClickMultiPlayButton()
+    {
+
+    }
+
+    public void OnClickSettingsButton()
+    {
+
+    }
+}
